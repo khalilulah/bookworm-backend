936e002ad30adbcd1bb9a8fec0da44a7132d2e53
khalilulah <alausakhalil@gmail.com>
2025-07-28 04:48:18 -0900

    update the login error message

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -90,8 +90,6 @@ export const signIn = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 };
